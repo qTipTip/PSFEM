0132c8f4cb8bed304c3cc2eb65d01b881e4663d1
Ivar Stangeby <istangeby@gmail.com>
2018-01-18 10:38:21 +0100

    Added test for incident triangles.

---
+++ tests/test_mesh.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pytest
+
+from PSFEM.mesh import Mesh
+
+
+def test_incident_triangles():
+
+    vertices = np.array([
+        [0, 0],
+        [1, 0],
+        [0, 1],
+        [1, 1],
+        [0.5, 0.5]
+    ])
+
+    triangles = np.array([
+        [0, 1, 4],
+        [1, 3, 4],
+        [3, 2, 4],
+        [2, 0, 4]
+    ])
+
+    M = Mesh(vertices, triangles)
+
+    expected_triangles = [0, 1, 2, 3]
+    computed_triangles = M.incident_triangles(4)
+
+    np.testing.assert_array_almost_equal(computed_triangles, expected_triangles)
+
+    expected_triangles = [2, 3]
+    computed_triangles = M.incident_triangles(2)
+
+    np.testing.assert_array_almost_equal(computed_triangles, expected_triangles)
