791220e111aa0fe05e2ee541715ef2957b54b757
Ivar Stangeby <istangeby@gmail.com>
2018-01-18 11:13:47 +0100

    Added test for adjacent triangles.

--- tests/test_mesh.py
+++ tests/test_mesh.py
@@ -32,3 +32,39 @@ def test_incident_triangles():
     computed_triangles = M.incident_triangles(2)
 
     np.testing.assert_array_almost_equal(computed_triangles, expected_triangles)
+
+
+def test_adjacent_triangles():
+
+    vertices = np.array([
+        [0, 0],
+        [1, 0],
+        [0, 1],
+        [1, 1],
+        [0.5, 0.5]
+    ])
+
+    triangles = np.array([
+        [0, 1, 4],
+        [1, 3, 4],
+        [3, 2, 4],
+        [2, 0, 4]
+    ])
+
+    M = Mesh(vertices, triangles)
+
+    expected_triangles = [2, 3]
+    computed_triangles = M.adjacent_triangles(7)
+
+    np.testing.assert_array_almost_equal(computed_triangles, expected_triangles)
+
+    expected_triangles = [1, 2]
+    computed_triangles = M.adjacent_triangles(6)
+
+    np.testing.assert_array_almost_equal(computed_triangles, expected_triangles)
+
+    expected_triangles = [0]
+    computed_triangles = M.adjacent_triangles(0)
+
+    np.testing.assert_array_almost_equal(computed_triangles, expected_triangles)
+
