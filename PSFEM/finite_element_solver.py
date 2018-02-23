3b7ab7396dd18fec967c2d96d22f646384e2ad4a
Ivar Stangeby <istangeby@gmail.com>
2018-02-23 14:46:48 +0100

    Typo!!!

--- PSFEM/finite_element_solver.py
+++ PSFEM/finite_element_solver.py
@@ -8,7 +8,7 @@ from SSplines import gaussian_quadrature_data, gaussian_quadrature_ps12
 quad_points, quad_weights = gaussian_quadrature_data(2)
 
 
-def solve(a, L, V, verbose=False, nprocs=1):
+def solve(a, L, V, verbose=False, nprocs=1, integration_method=gaussian_quadrature_ps12):
     """
     Solves the discrete finite element problem
     Find u in V such that
@@ -33,12 +33,12 @@ def solve(a, L, V, verbose=False, nprocs=1):
         local_basis = [V.basis[basis_number].local_representation[triangle] for basis_number in l2g]
         for j in tqdm.trange(12, leave=False, disable=not verbose, desc='   Local assembly'):
             for i in range(j + 1):
-                I = gaussian_quadrature_ps12(triangle_coords, a(local_basis[i], local_basis[j]), B, W)
+                I = integration_method(triangle_coords, a(local_basis[i], local_basis[j]), B, W)
                 A[l2g[i], l2g[j]] += I
                 if i != j:
                     A[l2g[j], l2g[i]] += I
 
-            b[l2g[j]] += gaussian_quadrature_ps12(triangle_coords, (local_basis[j]), B, W)
+            b[l2g[j]] += integration_method(triangle_coords, L(local_basis[j]), B, W)
 
     for triangle in tqdm.trange(len(V.mesh.triangles), disable=not verbose, desc='Global assembly'):
         compute_single_triangle(triangle)
